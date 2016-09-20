aaa846431fd13a809910d6a38f736e4d833ef4e0
Caleb Oki <caleboki@gmail.com>
2016-09-20 16:11:52 +0100

    Completed Problem 10

---
+++ 10.php
@@ -0,0 +1,24 @@
+<?php
+/*https://projecteuler.net/problem=10*/
+
+function PrimeSum($last)
+{
+	$n = 2;
+	$r = range(2, $last);
+	$primes = array_combine($r, $r);
+
+	//looping thru the numbers and removing the multiples
+	while (pow($n, 2) < $last) {
+		
+		for ($i=$n; $i <= $last ; $i += $n ) {
+			if ($i == $n) {
+				continue;
+			}
+			unset($primes[$i]);
+		}
+		$n = next($primes);
+	}
+
+	return $primes;
+}
+print array_sum((PrimeSum(2000000))); //adding up the contents of the primes array
